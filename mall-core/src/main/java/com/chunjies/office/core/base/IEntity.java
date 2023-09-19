04d484baf1d3298fa80f02a87b24d01dddfeb038
weichunjie <chunjie@vip.126.com>
2023-09-19 17:35:16 +0800

    基类增加version字段

--- mall-core/src/main/java/com/chunjies/office/core/base/IEntity.java
+++ mall-core/src/main/java/com/chunjies/office/core/base/IEntity.java
@@ -12,12 +12,74 @@ public abstract class IEntity {
     private Long id;
     private String createPk;
     private String createBy;
-
     private Date createTime;
-
     private String modifyPk;
     private String modifyBy;
     private Date modifyTime;
+    //乐观锁设置，当前无用
+    private int version;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCreatePk() {
+        return createPk;
+    }
+
+    public void setCreatePk(String createPk) {
+        this.createPk = createPk;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getModifyPk() {
+        return modifyPk;
+    }
+
+    public void setModifyPk(String modifyPk) {
+        this.modifyPk = modifyPk;
+    }
+
+    public String getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(String modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
 }

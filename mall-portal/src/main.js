5906edd03aafd49f92a90b4310133b8be91cfdf0
chunjie <chunjie@vip.126.com>
2023-09-23 22:27:18 +0800

    js

--- mall-portal/src/main.js
+++ mall-portal/src/main.js
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import  '/assets/css/bootstrap.min.css'
-import  '/assets/css/main.css'
-import  '/assets/js/jquery.min.js'
-import  '/assets/js/bootstrap.min.js'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import '/assets/css/bootstrap.min.css'
+import '/assets/css/main.css'
+import '/assets/js/jquery.min.js'
+import '/assets/js/bootstrap.min.js'
 import App from './App.vue'
 import router from './router'
+
 const app = createApp(App)
 
 app.use(createPinia())
-
 app.use(router)
 app.mount('#app')

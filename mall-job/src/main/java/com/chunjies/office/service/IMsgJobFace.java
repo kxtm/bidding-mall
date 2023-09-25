2a50fb113f18f017e91783f71f0e740a95516ef8
weichunjie <chunjie@vip.126.com>
2023-09-25 14:13:24 +0800

    消息处理服务

---
+++ mall-job/src/main/java/com/chunjies/office/service/IMsgJobFace.java
@@ -0,0 +1,11 @@
+package com.chunjies.office.service;
+
+/**
+ * {@code @author} chunjie
+ * {@code @time} 2023/9/25
+ * {@code @description}
+ */
+public interface IMsgJobFace {
+
+    void getMsg();
+}
---
+++ mall-job/src/main/java/com/chunjies/office/service/impl/MsgJobFaceImpl.java
@@ -0,0 +1,18 @@
+package com.chunjies.office.service.impl;
+
+import com.chunjies.office.service.IMsgJobFace;
+import org.springframework.stereotype.Service;
+
+/**
+ * {@code @author} chunjie
+ * {@code @time} 2023/9/25
+ * {@code @description} 定时任务逻辑
+ */
+
+@Service("msgJobFace")
+public class MsgJobFaceImpl implements IMsgJobFace {
+    @Override
+    public void getMsg() {
+
+    }
+}

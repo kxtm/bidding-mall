bbef6bd71a12a50b286a7e006869f3ff7f9d109e
weichunjie <chunjie@vip.126.com>
2023-09-25 15:55:14 +0800

    增加供应商服务及支付服务

---
+++ mall-epp/src/main/java/com/chunjies/office/service/impl/PayFaceImpl.java
@@ -0,0 +1,17 @@
+package com.chunjies.office.service.impl;
+
+import com.chunjies.office.service.IPayFace;
+import org.springframework.stereotype.Service;
+
+/**
+ * {@code @author} chunjie
+ * {@code @time} 2023/9/25
+ * {@code @description}
+ */
+
+
+@Service("payFace")
+public class PayFaceImpl implements IPayFace {
+
+
+}
---
+++ mall-epp/src/main/java/com/chunjies/office/service/impl/SupplierFaceImpl.java
@@ -0,0 +1,14 @@
+package com.chunjies.office.service.impl;
+
+import com.chunjies.office.service.ISupplierFace;
+import org.springframework.stereotype.Service;
+
+/**
+ * {@code @author} chunjie
+ * {@code @time} 2023/9/25
+ * {@code @description}
+ */
+
+@Service("supplierFace")
+public class SupplierFaceImpl implements ISupplierFace {
+}

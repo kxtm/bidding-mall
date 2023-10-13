1fccf85eca81fc5c6c831d732f14c1f5ca975046
chunjie <chunjie@vip.126.com>
2023-10-13 21:16:34 +0800

    样式调整

--- mall-ui-portal/src/components/Header.vue
+++ mall-ui-portal/src/components/Header.vue
@@ -2,11 +2,21 @@
 </script>
 
 <template>
-<div class="container-fluid of-head">
-  <div class="container"> aaaaaaaa</div>
-</div>
+<header class="container-fluid header-top">
+  <div class="container">
+     <div class="col-sm-12 header-top-nav">
+         <div class="col-sm-2"><i class="glyphicon glyphicon-map-marker"></i>郑州分公司</div>
+
+     </div>
+  </div>
+  <div class="">
+
+  </div>
+</header>
 
 </template>
 <style scoped>
-.of-head{background:#e3e4e5;max-height: 30px;height: 30px;}
+.header-top{background:#e3e4e5;max-height: 30px;height: 30px;}
+.header-top-nav{padding:5px;}
+.glyphicon-map-marker{color: red;}
 </style>

a8f42b016fc81fa7324bb63789fc3cf8d06fcfc0
chunjie <chunjie@vip.126.com>
2023-10-16 17:12:30 +0800

    优化代码

--- mall-starter/src/main/java/com/chunjies/office/config/AuthInterceptor.java
+++ mall-starter/src/main/java/com/chunjies/office/config/AuthInterceptor.java
@@ -1,15 +1,14 @@
 package com.chunjies.office.config;
 
 
+import com.chunjies.office.core.base.Result;
 import com.chunjies.office.core.utils.JsonUtil;
 import com.chunjies.office.core.utils.JwtUtils;
-import com.chunjies.office.core.base.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
--- mall-starter/src/main/java/com/chunjies/office/config/GlobalAdvice.java
+++ mall-starter/src/main/java/com/chunjies/office/config/GlobalAdvice.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 /**
  * {@code @author:} chunjie
@@ -20,8 +21,8 @@ import javax.servlet.http.HttpServletRequest;
  **/
 
 @RestControllerAdvice
-public class GlobalHandler {
-    final Logger logger = LoggerFactory.getLogger(GlobalHandler.class);
+public class GlobalAdvice {
+    final Logger logger = LoggerFactory.getLogger(GlobalAdvice.class);
 
     @ExceptionHandler(Exception.class)
     public Result<String> handleException(Exception e, HttpServletRequest req) {
@@ -44,7 +45,7 @@ public class GlobalHandler {
     @ExceptionHandler(BindException.class)
     public Result<String> handleBindException(BindException e, HttpServletRequest req) {
         logger.error("bindException地址:{},异常信息", req.getRequestURL(), e);
-        return Result.error(e.getBindingResult().getFieldError().getDefaultMessage());
+        return Result.error(Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
     }
 
     @ExceptionHandler(BizException.class)
@@ -55,8 +56,8 @@ public class GlobalHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Result<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        logger.error("handleMethodArgumentNotValidException 异常信息:{}", e);
-        return Result.error(e.getBindingResult().getFieldError().getDefaultMessage());
+        logger.error("handleMethodArgumentNotValidException 异常信息:", e);
+        return Result.error(Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
     }
 
     @ExceptionHandler(ServletException.class)

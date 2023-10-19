f541d9c8e2a9ee3282880684682ea1a3dc45cf07
chunjie <chunjie@vip.126.com>
2023-10-19 19:07:56 +0800

    首页增加导航搜索

--- mall-ui-portal/src/components/Header.vue
+++ mall-ui-portal/src/components/Header.vue
@@ -2,21 +2,50 @@
 </script>
 
 <template>
-<header class="container-fluid header-top">
-  <div class="container">
-     <div class="col-sm-12 header-top-nav">
-         <div class="col-sm-2"><i class="glyphicon glyphicon-map-marker"></i>郑州分公司</div>
-
-     </div>
+  <header class="navbar navbar-default header-top-bar">
+    <div class="container">
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="/"><i class="glyphicon glyphicon-map-marker glyphicon-map"></i>中国人民财产保险股份有限公司阿克苏分公司普惠金融业务部销售团队</a>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">王天宇,您好</a></li>
+          <li><a href="#">收藏夹</a></li>
+          <li class="line"></li>
+          <li><a href="#">我的需求</a></li>
+          <li class="line"></li>
+          <li><a href="#">我的订单</a></li>
+          <li class="line"></li>
+          <li><a href="#">我的地址</a></li>
+          <li class="line"></li>
+          <li><a href="#">客户服务</a></li>
+          <li class="line"></li>
+          <li><a href="#">意见建议</a></li>
+          <li class="line"></li>
+          <li><a href="#">帮助中心</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
+  <div class="container header-search">
+    <div class="logo"><img src="/logo.jpg" alt="logo"></div>
+    <div class="col-lg-7  input-group search">
+      <input type="text" class="form-control search-input" placeholder="请输入搜索内容">
+      <span class="input-group-btn"><button class="btn btn-default search-btn" type="button"><i
+          class="glyphicon glyphicon-search"></i></button></span>
+    </div>
+    <div class="search-right">
+      <button class="btn btn-default"><i class="glyphicon glyphicon-shopping-cart"></i>购物车</button>
+    </div>
   </div>
-  <div class="">
-
+  <div class="cate-navbar">
+    <div class="container cate">
+      <div class="cate-list"><div class="cate-title"><i class="glyphicon glyphicon-list"></i>商品分类</div></div>
+      <div class="cate-channel">
+         <div>营销礼品</div>
+         <div>办公用品</div>
+      </div>
+    </div>
   </div>
-</header>
-
 </template>
-<style scoped>
-.header-top{background:#e3e4e5;max-height: 30px;height: 30px;}
-.header-top-nav{padding:5px;}
-.glyphicon-map-marker{color: red;}
-</style>

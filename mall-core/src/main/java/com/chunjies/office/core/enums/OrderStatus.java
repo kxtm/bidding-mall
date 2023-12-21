fff105fcaf68009248b99249553f877d3bd2db2c
chunjie <wei@chunjie.eblog.vip>
2023-12-21 16:28:29 +0800

    增加状态

--- mall-core/src/main/java/com/chunjies/office/core/enums/OrderStatus.java
+++ mall-core/src/main/java/com/chunjies/office/core/enums/OrderStatus.java
@@ -1,35 +1,24 @@
 package com.chunjies.office.core.enums;
 
+import lombok.Getter;
+
 /**
  * {@code @author} chunjie
  * {@code @time} 2023/9/18
  * {@code @description}
  */
+@Getter
 public enum OrderStatus {
-    FREEZE(-1, "冻结");
+    //新建、待确认、取消、待发货、已发货、已妥投、已验收、拒收、退货、换货、待对账、对账失败、待开票、已开票、待报销、已报销
+    DEFAULT(0, "新建"), CONFIRMING(1, "待确认"), CANCEL(-1, "取消"), SHIPPING(2, "待发货"), SHIPPED(3, "已发货"),
+    DELIVERED(4, "已妥投"), CHECKED(5, "已验收"), REJECTION(-2, "拒收"), RETURN_GOODS(-3, "退货"), EXCHANGE_GOODS(6, "换货");
 
-    private int status;
-
-    private String desc;
+    final int status;
+    final String desc;
 
     OrderStatus(int status, String desc) {
         this.status = status;
         this.desc = desc;
     }
 
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
 }
